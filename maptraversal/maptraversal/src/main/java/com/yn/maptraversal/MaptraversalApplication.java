e6ff1f3051c9bcaace3ba13f112b805a07ba8844
yn197 <1607401968@qq.com>
2023-04-25 14:17:48 +0800

    map遍历4种方式

---
+++ maptraversal/maptraversal/src/main/java/com/yn/maptraversal/MaptraversalApplication.java
@@ -0,0 +1,13 @@
+package com.yn.maptraversal;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class MaptraversalApplication {
+
+    public static void main(String[] args) {
+        SpringApplication.run(MaptraversalApplication.class, args);
+    }
+
+}
